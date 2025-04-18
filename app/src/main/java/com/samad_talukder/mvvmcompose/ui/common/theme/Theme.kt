60172c738330c82b340ec87105f9fca2b124b3c3
Samad Talukder <samadtalukdar6@gmail.com>
2025-04-19 00:42:52 +0600

    Refactor: Theme and Type file moved to ui layer

--- app/src/main/java/com/samad_talukder/mvvmcompose/ui/common/theme/Theme.kt
+++ app/src/main/java/com/samad_talukder/mvvmcompose/ui/common/theme/Theme.kt
@@ -1,6 +1,5 @@
-package com.samad_talukder.mvvmcompose.ui.theme
+package com.samad_talukder.mvvmcompose.ui.common.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
