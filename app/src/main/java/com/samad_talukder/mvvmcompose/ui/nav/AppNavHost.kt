ca1b53b120251cf276795a06e5a560a615f578b7
Samad Talukder <samadtalukdar6@gmail.com>
2025-05-22 18:06:25 +0800

    Refactored: Handle state with lifecycle

--- app/src/main/java/com/samad_talukder/mvvmcompose/ui/nav/AppNavHost.kt
+++ app/src/main/java/com/samad_talukder/mvvmcompose/ui/nav/AppNavHost.kt
@@ -11,7 +11,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import com.samad_talukder.mvvmcompose.ui.details.GithubUserDetailsScreen
+import com.samad_talukder.mvvmcompose.ui.features.details.GithubUserDetailsScreen
 import com.samad_talukder.mvvmcompose.ui.features.home.HomeScreen
 
 @Composable
